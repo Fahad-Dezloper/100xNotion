ef30fae437c20702601c5b31768d8fe4315aaea1
Fahad <fahad.khan2216@gmail.com>
2025-04-11 16:19:36 +0530

    folder removed

--- Notion100xDevsFe/app/components/auth/signout-button.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import { signOut } from "next-auth/react"
- 
-export function SignOut() {
-  return <button className="px-4 py-2 cursor-pointer bg-red-500 text-white rounded-lg shadow hover:bg-red-600 transition-transform transform hover:scale-105" onClick={() => signOut()}>Sign Out</button>
-}
